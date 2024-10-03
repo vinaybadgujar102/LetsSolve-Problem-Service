4ab185b711a26c577b7ed5d66be1482e7fa8bd65
Vinay Badgujar <vinaybadgujar8@gmail.com>
2024-10-03 14:47:43 +0530

    fix: migrate from native status codes to a library

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,25 +1,37 @@
+const { StatusCodes } = require("http-status-codes");
+
 function pingProblemController(req, res) {
   return res.send("Pong problem controller");
 }
 
 function addProblem(req, res) {
-  return res.status(501).json({ message: "Not implemented" });
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: "Not implemented" });
 }
 
 function getProblem(req, res) {
-  return res.status(501).json({ message: "Not implemented" });
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: "Not implemented" });
 }
 
 function getProblems(req, res) {
-  return res.status(501).json({ message: "Not implemented" });
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: "Not implemented" });
 }
 
 function deleteProblem(req, res) {
-  return res.status(501).json({ message: "Not implemented" });
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: "Not implemented" });
 }
 
 function updateProblem(req, res) {
-  return res.status(501).json({ message: "Not implemented" });
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: "Not implemented" });
 }
 
 module.exports = {
