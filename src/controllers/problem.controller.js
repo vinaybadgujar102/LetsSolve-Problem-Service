2775a2a55e9f54406b15c67c8da417f5ab4ecfc5
Vinay Badgujar <vinaybadgujar8@gmail.com>
2024-10-03 14:44:29 +0530

    Add:not implemented response to problem controllers

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -2,15 +2,25 @@ function pingProblemController(req, res) {
   return res.send("Pong problem controller");
 }
 
-function addProblem(req, res) {}
+function addProblem(req, res) {
+  return res.status(501).json({ message: "Not implemented" });
+}
 
-function getProblem(req, res) {}
+function getProblem(req, res) {
+  return res.status(501).json({ message: "Not implemented" });
+}
 
-function getProblems(req, res) {}
+function getProblems(req, res) {
+  return res.status(501).json({ message: "Not implemented" });
+}
 
-function deleteProblem(req, res) {}
+function deleteProblem(req, res) {
+  return res.status(501).json({ message: "Not implemented" });
+}
 
-function updateProblem(req, res) {}
+function updateProblem(req, res) {
+  return res.status(501).json({ message: "Not implemented" });
+}
 
 module.exports = {
   pingProblemController,
